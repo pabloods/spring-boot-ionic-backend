4e4386e5995d9688d48451eec46a5ee01af0341b
unknown <pablo_ods@hotmail.com>
2020-10-05 22:55:35 -0300

    Criando operaçao de instanciação

--- src/main/java/com/pablo/CursomcApplication.java
+++ src/main/java/com/pablo/CursomcApplication.java
@@ -1,13 +1,34 @@
 package com.pablo;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursomcApplication {
+import com.pablo.domain.Categoria;
+import com.pablo.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CursomcApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		Categoria cat1 = new Categoria(null, "Medicina");
+		Categoria cat2 = new Categoria(null, "Direito");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+	}
+	
+	
 
 }
