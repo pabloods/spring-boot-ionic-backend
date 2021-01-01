9a47624a1855ea6a4d716cb2e09027d99fe3b5bb
unknown <pablo_ods@hotmail.com>
2020-12-31 22:59:59 -0300

    Paginação com parametros opcionais na requisicao

--- src/main/java/com/pablo/services/CategoriaService.java
+++ src/main/java/com/pablo/services/CategoriaService.java
@@ -5,12 +5,16 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 import com.pablo.domain.Categoria;
 import com.pablo.repositories.CategoriaRepository;
 import com.pablo.services.exceptions.DataIntegrityException;
 import com.pablo.services.exceptions.ObjectNotFoundException;
+
+
 
 @Service
 public class CategoriaService {
@@ -43,13 +47,16 @@ public class CategoriaService {
 			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos.");
 		}
 		
-	}
-	
+	}	
 	
 	public List<Categoria> findAll(){
 		return repo.findAll();
 	}
-	
+		
+	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, org.springframework.data.domain.Sort.Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest); 		
+	}
 	
 	
 }
